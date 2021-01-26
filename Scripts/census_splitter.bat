a72e72dcd3c3b94c6dd1abd030b987f8a3d2934a
grayson <grayson@server101>
2021-01-26 00:46:26 -0500

    01/26/2021

--- Scripts/census_splitter.bat
+++ Scripts/census_splitter.bat
@@ -1,12 +1,17 @@
 @echo off
 
-REM Wowrking
+REM Working
 set arg1=%1
 set arg2=%2
 shift
+shift
 
 REM Loop Rscript for each File in Folder
-for %%f in %arg1% do call R-Portable\App\R-Portable\bin\Rscript.exe Scripts\census_splitter_io.R %%f %arg2%
+REM cd /d %arg1%
+for /f %%f in ('dir /s/b %arg1%') do call R-Portable\App\R-Portable\bin\Rscript.exe Scripts\census_splitter_io.R %%f %arg2%
+
+
+
 
 
 
