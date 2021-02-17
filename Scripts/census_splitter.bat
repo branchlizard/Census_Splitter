e335dc1830f2ee42c8f8f7b2ec25db218a3e1dc0
grayson <grayson@server101>
2021-02-17 12:20:30 -0500

    02/17/2021

--- Scripts/census_splitter.bat
+++ Scripts/census_splitter.bat
@@ -1,13 +1,23 @@
 @echo off
+setlocal enabledelayedexpansion
 
-REM Working
+REM Parameters
 set arg1=%1
 set arg2=%2
 shift
 shift
 
-REM Loop Rscript for each File in Folder
+REM Remove all spaces from .csv files only
+cd %arg1%
+for %%a in (* *) do (
+	set file=%%a
+	ren "!file!" "!file: =_!"
+	)
+
+REM CD to working directory
 cd "%UserProfile%\Documents\Census_Splitter\"
+
+REM Loop Rscript for each File in Folder
 for /f %%f in ('dir /s/b %arg1%') do call R-Portable\App\R-Portable\bin\Rscript.exe Scripts\census_splitter_io.R %%f %arg2%
 
 
